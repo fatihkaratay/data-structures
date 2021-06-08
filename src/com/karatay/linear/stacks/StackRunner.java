386b091592b7e313bf4d9a7dfd991e0efe7e450c
Fatih Karatay <fatih.karatay@ngc.com>
2021-06-08 15:31:28 -0400

    stack runner class is added

---
+++ src/com/karatay/linear/stacks/StackRunner.java
@@ -0,0 +1,24 @@
+package com.karatay.linear.stacks;
+
+public class StackRunner {
+    public static void main(String[] args) {
+        StackExercises helper = new StackExercises();
+
+        String str0 = null; // Illegal arg exc.
+        String str1 = ""; //T
+        String str2 = "()";// T
+        String str3 = "(]"; //F
+        String str4 = "(1 + 2)"; //T
+        String str5 = "(1) + (2)"; //T
+        String str6 = "{[(1) + (2)]}"; // T
+        String str7 = "{[(5s)}"; // F
+//        System.out.println(helper.isBalanced(str0));
+        System.out.println(helper.isBalanced(str1));
+        System.out.println(helper.isBalanced(str2));
+        System.out.println(helper.isBalanced(str3));
+        System.out.println(helper.isBalanced(str4));
+        System.out.println(helper.isBalanced(str5));
+        System.out.println(helper.isBalanced(str6));
+        System.out.println(helper.isBalanced(str7));
+    }
+}
