e1b0bcc4788db3e230eb3542bc8c964e070bab7f
Fatih Karatay <fatih.karatay@ngc.com>
2021-06-08 15:24:32 -0400

    hashmap exercises class is added

---
+++ src/com/karatay/linear/hashmaps/HashMapExercises.java
@@ -0,0 +1,53 @@
+package com.karatay.linear.hashmaps;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMapExercises {
+    // 1-Find first non repeated character.
+    public char getFirstNonRepeated(String str) {
+        if (str == null || str.length() == 0) throw new IllegalArgumentException();
+
+        str = formatStr(str);
+        Map<Character, Integer> map = new HashMap<>();
+        char[] chars = str.toCharArray();
+
+        for (char ch : chars) {
+            int count = map.containsKey(ch) ? map.get(ch) : 0;
+            map.put(ch, count + 1);
+        }
+
+        for (char ch : chars)
+            if (map.get(ch) == 1)
+                return ch;
+
+        return Character.MIN_VALUE;
+    }
+
+    // 2-Find first repeated character.
+    public char getFirstRepeated(String str) {
+        str = formatStr(str);
+        Set<Character> seen = new HashSet<>();
+
+        for (char ch : str.toCharArray()) {
+            if (seen.contains(ch))
+                return ch;
+
+            seen.add(ch);
+        }
+
+        return Character.MIN_VALUE;
+    }
+
+    private String formatStr(String str) {
+        String[] tempStr = str.toLowerCase().split(" ");
+        StringBuilder sb = new StringBuilder();
+
+        for (String item : tempStr)
+            sb.append(item);
+
+        return sb.toString();
+    }
+}
