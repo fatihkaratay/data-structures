2c83767e40c2afec4cd727000ef4da29431ab23f
Fatih Karatay <fatih.karatay@ngc.com>
2021-06-08 15:43:09 -0400

    Binary tree class is added

--- src/com/karatay/hierarchical/trees/BinaryTree.java
+++ src/com/karatay/hierarchical/trees/BinaryTree.java
@@ -1,4 +1,12 @@
 package com.karatay.hierarchical.trees;
 
 public class BinaryTree {
+    private Node root;
+
+    public void insert(int value) {
+        if (root == null) {
+            root = new Node(value);
+            return;
+        }
+    }
 }
