243913189576675f668b4710a64ec3a9b4a794f8
Fatih Karatay <fatih.karatay@ngc.com>
2021-06-09 16:19:03 -0400

    is binary search tree checker is implemented

--- src/com/karatay/hierarchical/trees/BinaryTree.java
+++ src/com/karatay/hierarchical/trees/BinaryTree.java
@@ -156,7 +156,24 @@ public class BinaryTree {
         return false;
     }
 
+    public boolean isBinarySearchTree() {
+        return isBinarySearchTree(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    private boolean isBinarySearchTree(Node root, int min, int max) {
+        if (root == null)
+            return true;
+        if (root.value > max || root.value < min)
+            return false;
+        return isBinarySearchTree(root.leftChild, min, root.value - 1) &&
+                isBinarySearchTree(root.rightChild, root.value + 1, max);
+    }
+
     private boolean isLeafNode(Node root) {
         return root.leftChild == null && root.rightChild == null;
+    }
+    public void swapRoot() {
+        Node temp = root.leftChild;
+        root.leftChild = root.rightChild;
+        root.rightChild = temp;
     }
 }
--- src/com/karatay/hierarchical/trees/BinaryTreeRunner.java
+++ src/com/karatay/hierarchical/trees/BinaryTreeRunner.java
@@ -12,15 +12,19 @@ public class BinaryTreeRunner {
         tree1.insert(10);
 
         BinaryTree tree2 = new BinaryTree();
-        tree2.insert(7);
-        tree2.insert(4);
-        tree2.insert(9);
-        tree2.insert(1);
+        tree2.insert(20);
+        tree2.insert(10);
+        tree2.insert(30);
         tree2.insert(6);
+        tree2.insert(21);
+        tree2.insert(4);
+        tree2.insert(3);
         tree2.insert(8);
-        tree2.insert(11);
 
-        System.out.println(tree1.isEqual(tree2));
+        tree1.swapRoot();
+
+        System.out.println(tree1.isBinarySearchTree());
+        System.out.println(tree2.isBinarySearchTree());
 
 
     }
