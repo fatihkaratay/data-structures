e9098e9e8f1f435c987a8f8e4c5eef99eee5738b
Fatih Karatay <fatihkaratay@gmail.com>
2021-06-21 16:23:48 -0400

    get kth largest node using PQ

--- src/com/karatay/hierarchical/heaps/HeapRunner.java
+++ src/com/karatay/hierarchical/heaps/HeapRunner.java
@@ -1,10 +1,37 @@
 package com.karatay.hierarchical.heaps;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class HeapRunner {
     public static void main(String[] args) {
-       int[] numbers = { 5, 3, 8, 4, 1, 2};
-      System.out.println(MaxHeap.getKthLargest(numbers, 6));
+        int[] numbers = { 5, 3, 8, 4, 1, 2};
+        System.out.println(MaxHeap.getKthLargest(numbers, 3));
+        System.out.println(getKthLargest(numbers, 3));
+        System.out.println(getKthSmallest(numbers, 1));
+    }
+
+    private static int getKthLargest(int[] numbers, int k) {
+        if (k < 1 || k > numbers.length)
+            throw new IllegalArgumentException();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        for (int number : numbers)
+            maxHeap.add(number);
+        for (int i = 0; i < k - 1; i++)
+            maxHeap.poll();
+
+        return maxHeap.peek();
+    }
+
+    private static int getKthSmallest(int[] numbers, int k) {
+        if (k < 1 || k > numbers.length)
+            throw new IllegalArgumentException();
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int number : numbers)
+            minHeap.add(number);
+        for (int i = 0; i < k - 1; i++)
+            minHeap.poll();
+
+        return minHeap.peek();
     }
 }
