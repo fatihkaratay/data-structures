ff823f7e88c558f014067420a7fd3b5889b12a63
Fatih Karatay <fatihkaratay@gmail.com>
2021-06-21 15:27:14 -0400

    testing

--- src/com/karatay/hierarchical/heaps/HeapRunner.java
+++ src/com/karatay/hierarchical/heaps/HeapRunner.java
@@ -2,10 +2,14 @@ package com.karatay.hierarchical.heaps;
 
 public class HeapRunner {
     public static void main(String[] args) {
+        int[] numbers = {5, 3, 10, 1, 4, 2};
         Heap heap = new Heap();
-        heap.insert(10);
-        heap.insert(5);
-        heap.insert(17);
+
+        for (int number : numbers)
+            heap.insert(number);
+
+        while (!heap.isEmpty())
+            System.out.println(heap.remove());
 
         System.out.println("DONE");
     }
