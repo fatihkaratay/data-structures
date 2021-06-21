88dfda81a0997804a904bd8e69581366c079ece2
Fatih Karatay <fatihkaratay@gmail.com>
2021-06-21 15:13:06 -0400

    remove method is added

--- src/com/karatay/hierarchical/heaps/Heap.java
+++ src/com/karatay/hierarchical/heaps/Heap.java
@@ -11,6 +11,48 @@ public class Heap {
         bubbleUp();
     }
 
+    public void remove() {
+        if (isEmpty()) throw new IllegalStateException();
+        items[0] = items[--size];
+
+        int index = 0;
+        while (index <= size && !isValidParent(index)) {
+            int largerChildIndex = largerChildIndex(index);
+            swap(index, largerChildIndex);
+            index = largerChildIndex;
+        }
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private int largerChildIndex(int index) {
+        return (leftIndex(index) > rightIndex(index)) ?
+                leftIndex(index) :
+                rightIndex(index);
+    }
+
+    private boolean isValidParent(int index) {
+        return  items[index] >= leftChild(index) &&
+                items[index] >= rightChild(index);
+    }
+
+    private int rightChild(int index) {
+        return items[rightIndex(index)];
+    }
+
+    private int leftChild(int index) {
+        return items[leftIndex(index)];
+    }
+
+    private int leftIndex(int index) {
+        return index * 2 + 1;
+    }
+    private int rightIndex(int index) {
+        return index * 2 + 2;
+    }
+
     public boolean isFull() {
         return size == items.length;
     }
