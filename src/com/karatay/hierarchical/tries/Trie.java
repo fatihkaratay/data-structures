4496411d6b80f3d29940328edfdb2d30437be2f9
Fatih Karatay <fatihkaratay@gmail.com>
2021-06-22 06:32:02 -0400

    Trie class is implemented.

---
+++ src/com/karatay/hierarchical/tries/Trie.java
@@ -0,0 +1,34 @@
+package com.karatay.hierarchical.tries;
+
+public class Trie {
+    private static int ALPHABET_SIZE = 26;
+
+    private class Node {
+        private char value;
+        private Node[] children = new Node[ALPHABET_SIZE];
+        private boolean isEndOfWord;
+
+        public Node(char value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Value=" + value;
+        }
+    }
+
+    private Node root = new Node(' ');
+
+    public void insert(String word) {
+        Node current = root;
+        for (char ch : word.toCharArray()) {
+            int index = ch - 'a';
+            if (current.children[index] == null) {
+                current.children[index] = new Node(ch);
+            }
+            current = current.children[index];
+        }
+        current.isEndOfWord = true;
+    }
+}
