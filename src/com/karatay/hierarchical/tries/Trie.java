bb7dedf2b78ddaf18192dfe2bcd71ba6b918589f
Fatih Karatay <fatihkaratay@gmail.com>
2021-06-22 06:39:58 -0400

    used hashmap to store the children.

--- src/com/karatay/hierarchical/tries/Trie.java
+++ src/com/karatay/hierarchical/tries/Trie.java
@@ -1,11 +1,13 @@
 package com.karatay.hierarchical.tries;
 
+import java.util.HashMap;
+
 public class Trie {
     private static int ALPHABET_SIZE = 26;
 
     private class Node {
         private char value;
-        private Node[] children = new Node[ALPHABET_SIZE];
+        private HashMap<Character, Node> children = new HashMap<>();
         private boolean isEndOfWord;
 
         public Node(char value) {
@@ -23,11 +25,10 @@ public class Trie {
     public void insert(String word) {
         Node current = root;
         for (char ch : word.toCharArray()) {
-            int index = ch - 'a';
-            if (current.children[index] == null) {
-                current.children[index] = new Node(ch);
+            if (current.children.get(ch) == null) {
+                current.children.put(ch, new Node(ch));
             }
-            current = current.children[index];
+            current = current.children.get(ch);
         }
         current.isEndOfWord = true;
     }
