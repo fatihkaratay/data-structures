cdfbe20a5417c820a9752ca5b111d66e3a81e24f
Fatih Karatay <fatihkaratay@gmail.com>
2021-06-29 16:42:18 -0400

    runner class is added

--- src/com/karatay/hierarchical/graphs/GraphRunner.java
+++ src/com/karatay/hierarchical/graphs/GraphRunner.java
@@ -1,4 +1,13 @@
 package com.karatay.hierarchical.graphs;
 
 public class GraphRunner {
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("C");
+
+        graph.addEdge("A", "B", 3);
+        graph.addEdge("A", "C", 2);
+    }
 }
